fd7109b74023254810880b756be02bcbe363bced
jay <jayamurugancbe@gmail.com>
2024-05-22 18:50:06 +0200

    new packages

--- docker/build/build_image.sh
+++ docker/build/build_image.sh
@@ -2,15 +2,19 @@
 
 distro_name=$1
 
-echo $distro_name
+echo "Building ros$distro_name using Dockerfile.ros2_$distro_name"
+echo "source /opt/ros/$distro_name/setup.bash" >> bash_lines.txt
 
-if [ -d ~/Documents/docker_storage/ros_$distro_name ]  # Check if directory exists
+# Check if directory exists
+if [ -d ~/Documents/docker_storage/ros_$distro_name ]  
 then
 	echo "Folder exists"
 else
-	mkdir ~/Documents/docker_storage/ros_$distro_name
+	mkdir -p ~/Documents/docker_storage/ros_$distro_name/ros2_ws/src/
 fi
 
 sudo docker build -t ros:$distro_name -f Dockerfile.ros2_$distro_name .
+
+head -n -1 bash_lines.txt > temp.txt ; mv temp.txt bash_lines.txt
 
 
--- docker/ros.sh
+++ docker/ros.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 IFS=' ' read -r VARDPY VARPROTO VARHEX <<< $(xauth list)
-container_name="ros-$1"
+distro_name=$1
+container_name="ros-$distro_name"
 
 if [ "$(sudo docker ps -a --quiet --filter status=running --filter name=$container_name)" ]; then
 	echo "Attaching into $container_name"
@@ -8,12 +9,12 @@ if [ "$(sudo docker ps -a --quiet --filter status=running --filter name=$contain
     exit 0
 fi
 
-docker_args+=("-v ~/Documents/docker_storage/ros_$1:/home")
+docker_args+=("-v ~/Documents/docker_storage/$container_name:/home")
 sudo docker run -it --privileged --rm \
 			--net=host \
 			--env="DISPLAY"\
 			-e VARDPY=$VARDPY -e VARPROTO=$VARPROTO -e VARHEX=$VARHEX \
-			-v ~/Documents/docker_storage/ros_foxy:/home \
+			-v ~/Documents/docker_storage/$container_name:/home \
 			--device=/dev/dri:/dev/dri \
-			--name="$container_name" ros:foxy
+			--name="$container_name" ros:$distro_name
 
