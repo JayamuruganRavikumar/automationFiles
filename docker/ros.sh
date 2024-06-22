7cab36fbe14b9880bae76527f5e12c4b4c6d8ddd
Jay <jayamurugancbe@gmail.com>
2024-06-22 18:24:24 +0200

    reset

--- docker/ros.sh
+++ docker/ros.sh
@@ -12,13 +12,14 @@ fi
 
 if [ "$nvidia" -eq 1 ] && [ ! -z "$nvidia" ];then
 	container_name="ros-$distro_name-nvidia"
-	echo "$(tput setaf 6)--------Running ros-$distro_name container--------$(tput sgr0)"
+	echo "$(tput setaf 6)--------Using $container_name as container name--------$(tput sgr0)"
 else
-	echo "$(tput setaf 6)--------Defaulting to ros-$distro_name container--------$(tput sgr0)"
 	container_name="ros-$distro_name"
+	echo "$(tput setaf 6)--------Using $container_name as container name--------$(tput sgr0)"
 fi
 
 if [ "$(sudo docker ps -a --quiet --filter status=running --filter name=$container_name)" ]; then
+	echo "$(tput setaf 1)--------Found runnning $container_name container--------$(tput sgr0)"
 	echo "$(tput setaf 6)--------Attaching into existing $container_name--------$(tput sgr0)"
     sudo docker exec -i -t \
 		--user="$distro_name" \
@@ -31,17 +32,15 @@ fi
 start_docker(){
 
 	local name=$1
-	local distro=$2
 	docker_args+=("-e NVIDIA_DRIVER_CAPABILITIES=all")
 	docker_args+=("-e NVIDIA_VISIBLE_DEVICES=all")
 	sudo docker run -it --privileged --rm \
 				--gpus all \
 				--net=host \
 				--env="DISPLAY"\
-				--user="$distro" \
 				-e VARDPY=$VARDPY -e VARPROTO=$VARPROTO -e VARHEX=$VARHEX \
 				"${docker_args[@]}" \
-				-v ~/Documents/docker_storage/$container_name:/home/$distro \
+				-v ~/Documents/docker_storage/$container_name:/home/ \
 				--device=/dev/dri:/dev/dri \
 				--name="$container_name" ros:"$name" \
 				/bin/bash
@@ -51,7 +50,7 @@ docker_args=()
 
 if [ "$nvidia" -eq 1 ];then
 
-	start_docker "$distro_name-nvidia" "$distro_name"
+	start_docker "$distro_name-nvidia"
 
 else
 	start_docker "$distro_name"
